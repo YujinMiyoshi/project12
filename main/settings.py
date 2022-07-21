e5005314b4763c0bd1d1bb3b3c635bc3adcba677
eugene <eugene.earth.solar@gmail.com>
2022-07-21 11:02:35 +0900

    register migrate

--- main/settings.py
+++ main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo',
+    'register',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,8 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+AUTH_USER_MODEL = 'register.User'
 
 LOGIN_URL = 'login'
 
